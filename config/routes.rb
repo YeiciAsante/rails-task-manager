039c5bae2fb1889bf479347426ef90e9df28cc70
Juan Salazar <jc.salazargomez@gmail.com>
2021-11-04 19:13:28 -0500

    routes incomplete

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # read
   get 'tasks', to: 'tasks#index'
-  # get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id', to: 'tasks#show', as: :task
 
   # # create
   # get 'tasks/new', to: 'tasks#new'
